2cdd3eac381f581ed0e27096da37628c5f4b53f5
James Barnes <James.Barnes@bcx.co.za>
2016-08-26 14:31:16 +0200

    Update names and instructions for lesson 1

---
+++ js/lesson1.js
@@ -0,0 +1,38 @@
+/*
+Module 3 - Lesson 1
+-------------------------------------------------------------------------------
+
+Lesson Exersize
+----------------------------------
+
+
+
+Notes
+-----------------------------------
+Use JavaScript to make the background of the index.html header change every 5 seconds.
+
+Use:
+    element = document.getElementById(id);
+    and
+    element.style.property = "value";
+
+to change line 55 of index.html which is:
+<header id="top" class="header" style="background: url(img/01.jpeg) no-repeat center center scroll;">
+
+
+
+Use:
+    setInterval(function,milliseconds);
+
+to make a function every so many milliseconds.
+
+Example:
+    setInterval(function(){alert("Hi there");}, 3000);
+
+This will create an alert every 3 seconds that says "Hi there".
+
+*/
+
+if (document.getElementById("top").style.background === 'url("img/01.jpeg") center center no-repeat scroll') {
+  alert("It is true");
+}
